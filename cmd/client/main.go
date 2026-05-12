7c46166f3e97ced2de95dc3f2514c6de434bb97f
Anna Muraveva <tomuchnure@gmail.com>
2026-05-12 16:47:48 +0700

    feat: bubble tea интерфейс добавлен

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,15 +1,131 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"messenger/protocol"
 	"net"
-	"os"
 	"strings"
 	"time"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
+
+type model struct {
+	viewport viewport.Model //окно с текстом
+	textarea textarea.Model //поле для ввода
+	messages []string       //локальный кэш
+	conn     net.Conn       //соединение
+	username string
+	err      error
+}
+
+// спец тип для интерфейса
+type serverMsg protocol.Message
+
+// инициализация
+func InitialModel(conn net.Conn, username string) model {
+	ta := textarea.New()
+	ta.Placeholder = "Кому:сообщение..."
+	ta.Focus()
+	ta.CharLimit = 280
+	ta.SetHeight(3)
+	vp := viewport.New(80, 15)
+	vp.SetContent("--- Соединение установлено. Добро пожаловать! ---\n")
+
+	return model{
+		textarea: ta,
+		viewport: vp,
+		conn:     conn,
+		username: username,
+		messages: []string{},
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink //моргающий курсор
+}
+
+// обновление
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+	//обработка типов событий
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			input := m.textarea.Value()
+			if input == "" {
+				return m, nil
+			}
+
+			parts := strings.SplitN(input, ":", 2)
+			if len(parts) < 2 {
+				m.messages = append(m.messages, lipgloss.NewStyle().Foreground(lipgloss.Color("9")).Render("Ошибка! Формат 'Имя: текст'"))
+				m.viewport.SetContent(strings.Join(m.messages, "\n"))
+				m.textarea.Reset()
+				return m, nil
+			}
+
+			pMsg := protocol.Message{
+				Sender:    m.username,
+				Recipient: strings.TrimSpace(parts[0]),
+				Content:   strings.TrimSpace(parts[1]),
+				Action:    protocol.ActionSendMessage,
+				TimeStamp: time.Now().Unix(),
+			}
+			protocol.WritePacket(m.conn, 2, pMsg)
+
+			m.messages = append(m.messages, fmt.Sprintf("Я -> %s: %s", pMsg.Recipient, pMsg.Content))
+			m.viewport.SetContent(strings.Join(m.messages, "\n"))
+			m.viewport.GotoBottom()
+			m.textarea.Reset()
+		}
+
+	case serverMsg:
+		newMsg := fmt.Sprintf("[%s]: %s", msg.Sender, msg.Content)
+		m.messages = append(m.messages, newMsg)
+		m.viewport.SetContent(strings.Join(m.messages, "\n"))
+		m.viewport.GotoBottom()
+	}
+	return m, tea.Batch(tiCmd, vpCmd)
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("Чат: %s\n\n%s\n\n%s\n%s", m.username,
+		m.viewport.View(),
+		m.textarea.View(),
+		lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render("Ctrl+C - выход | Enter - отправить"),
+	)
+}
+
+// горутина для чтения из сети
+func waitForMessages(conn net.Conn, p *tea.Program) {
+	for {
+		msgType, payload, err := protocol.ReadPacket(conn)
+		if err != nil {
+			break
+		}
+		if msgType == 2 {
+			var incoming protocol.Message
+			json.Unmarshal(payload, &incoming)
+
+			p.Send(serverMsg(incoming))
+		}
+	}
+}
 
 func main() {
 	conn, err := net.Dial("tcp", "localhost:8080")
@@ -17,60 +133,17 @@ func main() {
 		fmt.Println("Не удалось подключиться к серверу:", err)
 		return
 	}
-	defer conn.Close()
-
-	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Print("Введите Ваш никнейм: ")
-	username, _ := reader.ReadString('\n')
-	username = strings.TrimSpace(username)
+	var username string
+	fmt.Scanln(&username)
+	protocol.WritePacket(conn, 1, protocol.Message{Sender: username})
 
-	authMsg := protocol.Message{Sender: username}
+	p := tea.NewProgram(InitialModel(conn, username), tea.WithAltScreen())
 
-	protocol.WritePacket(conn, 1, authMsg)
-	fmt.Printf("Вы вошли как %s\n", username)
-
-	go func() {
-		for {
-			msgType, payload, err := protocol.ReadPacket(conn)
-			if err != nil {
-				fmt.Println("\nСвязь с сервером потеряна:")
-				os.Exit(1)
-			}
-
-			if msgType == 2 {
-				var incoming protocol.Message
-				json.Unmarshal(payload, &incoming)
-				fmt.Printf("\n[%s]: %s\n> ", incoming.Sender, incoming.Content)
-			}
-
-		}
-	}()
-
-	fmt.Println("Для отправки введите: получатель:сообщение")
-	for {
-		fmt.Print("> ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-
-		parts := strings.SplitN(input, ":", 2)
-
-		recipient := strings.TrimSpace(parts[0])
-		content := strings.TrimSpace(parts[1])
-
-		msg := protocol.Message{
-			Sender:    username,
-			Recipient: recipient,
-			Content:   content,
-			TimeStamp: time.Now().Unix(),
-			Action:    protocol.ActionSendMessage,
-		}
-
-		err := protocol.WritePacket(conn, 2, msg)
-		if err != nil {
-			fmt.Println("Ошибка при отправке пакета:", err)
-			break
-		}
+	go waitForMessages(conn, p)
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
 	}
 
 }
